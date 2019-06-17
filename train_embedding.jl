74df634c0891a11d5690a88b538e2f2b54586ac1
Austin Bean <austinbean@gmail.com>
2019-06-17 10:43:23 -0400

    intermediate steps towards embedding

--- train_embedding.jl
+++ train_embedding.jl
@@ -1,14 +1,21 @@
 # NLP
 
 
-using Word2Vec
+using Word2Vec, DataFrames, CSVFiles
+
+#=
+- Should do more pre-processing: remove "-", "1/2", ".", ";", ")", "("
+- Probably want something like "1/2" -> ".5", but the others can be dropped
+
+=#
+
 
 # produces the file diet_embed
 	word2vec("diet_text.txt", "diet_embed", verbose=true)
 	# not many words - 395 unique, 55,000 total.
 
 
-# Load the embeddings	
+# Load the embeddings
 embed_1 = wordvectors("./diet_embed")
 
 
@@ -24,6 +31,28 @@ embed_1 = wordvectors("./diet_embed")
 	# for the most part, those things "look right" - other words are similar
 
 #=
-I think what I need to do now is assign these words to the diet variables, then 
-figure out how to load the representations / outputs into the library.  
+I think what I need to do now is assign these words to the diet variables, then
+figure out how to load the representations / outputs into the library.
 =#
+
+diet_df = DataFrame(load("data.csv"))
+
+# want... something like a space-separated string of the numbers?
+
+"""
+`embedder(x,y)`
+Returns a vector `outp` of strings of the 
+"""
+function embedder(x,y)
+	outp = ""
+	for wd in 1:10
+		outp = outp*" "*y.words[wd]
+	end
+	return outp
+end
+
+
+
+
+
+outp = Dict(string(i) => )
