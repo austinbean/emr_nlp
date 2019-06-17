44050b1ef10f582c322cba1ef64fc054bca1f157
Austin Bean <austinbean@gmail.com>
2019-06-17 13:27:33 -0400

    first version completed through saving embedded data

--- train_embedding.jl
+++ train_embedding.jl
@@ -9,44 +9,14 @@ using Word2Vec, DataFrames, CSVFiles
 
 =#
 
-
-# produces the file diet_embed
-	word2vec("diet_text.txt", "diet_embed", verbose=true)
-	# not many words - 395 unique, 55,000 total.
-
-
-# Load the embeddings
-embed_1 = wordvectors("./diet_embed")
-
-
-# look at some items
-	get_vector(embed_1, "NEOSURE")
-	get_vector(embed_1, "BREAST")
-
-# cosine similar words
-
-	cosine_similar_words(embed_1, "NEOSURE")
-	cosine_similar_words(embed_1, "SIMILAC")
-	cosine_similar_words(embed_1, "BREAST")
-	# for the most part, those things "look right" - other words are similar
-
-#=
-I think what I need to do now is assign these words to the diet variables, then
-figure out how to load the representations / outputs into the library.
-=#
-
-diet_df = DataFrame(load("data.csv"))
-
-# want... something like a space-separated string of the numbers?
-
 """
-`embedder(x,y)`
+`vec_embedder(x,y)`
 y should be a WordVectors{String, Float64, Int64}
 x will be an long string
 Returns a vector `outp` of floats of the embeddings of words from `x` in the embedding `y`
 This should return a vector of floats.  This has variable length, depending on the length of x.
 """
-function embedder(x,y)
+function vec_embedder(x,y)
 	outp = Vector{Float64}()         # empty arrray
 	for wd in split(x)               # split the string input
 		if haskey( y.vocab_hash, wd) # need to catch key errors, I guess?
@@ -57,4 +27,68 @@ function embedder(x,y)
 end
 
 
-outp = Dict(string(i) => )
+"""
+`embed_set`
+will take a dataframe (of strings and quantities) recorded in x
+the columns of y are :diet and :total_quantity
+will return outp, a vector which is (recorded_consumption, vector_of_word_embeddings)
+"""
+function embed_set(x,y)
+    outp = Vector{Vector{Number}}()
+		# TODO - wrong size taken here, probably.
+	len, wid = size(x)
+	for el in 1:len
+		push!(outp, vcat(x[el,:total_quantity] , vec_embedder(x[el,:diet] ,y)) )
+	end
+	return outp
+end
+
+
+"""
+`generate_out(df_1)`
+takes the output of embed_set which is a vector of vectors and then
+retuns a single array of dimensions (length(df_1)) × (max length(df_1[i]) ∀ i = 1:length(df_1))
+Will generate a lot of trailing zeros
+"""
+function generate_out(df_1)
+	int1 = maximum([length(x) for x in df_1])
+	output_1 = zeros(length(df_1), int1)
+	for i = 1:length(df_1)
+		for j = 1:length(df_1[i])
+			output_1[i,j] += df_1[i][j]
+		end
+	end
+	return output_1
+end
+
+
+# produces the file diet_embed
+	word2vec("diet_text.txt", "diet_embed", verbose=true)
+	# not many words - 395 unique, 55,000 total.
+# Load the embeddings
+	embed_1 = wordvectors("./diet_embed")
+# Load the data to write some vectors of embedded words
+	diet_df = DataFrame(load("data.csv"))
+# map the words to (consumption, embedded_words)
+	df_out = embed_set(diet_df, embed_1)
+# generate a matrix from the above
+	to_save = generate_out(df_out)
+# convert the above array to a DataFrame to save the CSV
+	to_save = convert(DataFrame, to_save)
+# save the CSV
+	save("embedded_data.csv", to_save)
+
+
+
+#=
+# look at some items
+	get_vector(embed_1, "NEOSURE")
+	get_vector(embed_1, "BREAST")
+
+# cosine similar words
+
+	cosine_similar_words(embed_1, "NEOSURE")
+	cosine_similar_words(embed_1, "SIMILAC")
+	cosine_similar_words(embed_1, "BREAST")
+	# for the most part, those things "look right" - other words are similar
+=#
