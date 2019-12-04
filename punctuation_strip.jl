d4cc30d762bdbb5e0ae06e43524241c48efd75a0
Austin B. Bean <austinbean@gmail.com>
2019-12-04 13:55:24 -0500

    finishing, except for dashes

--- punctuation_strip.jl
+++ punctuation_strip.jl
@@ -3,91 +3,16 @@
 #=
 
 Julia standard: http://www.pcre.org/current/doc/html/pcre2syntax.html
-
-need to get matches, then replace or substitute or whateve.
-
-	keep note_text visit_id //gen text_comp = note_text 
-	* rewrite this to introduce spaces around some punctuation marks. 
-	replace note_text = lower(note_text) 
-	* Here need to do this with USTR regexes. 
-
-	* handle annoying strings of question marks. 
-	replace note_text = ustrregexra(note_text, "\?{2,}", "?") // sequences of ? down to 1 
-	replace note_text = ustrregexra(note_text, "\?", " ") // remove all single ? 
-	replace note_text = subinstr(note_text, "?", "", .) // this is just a check - doesn't end up doing anything. 
+	X replace note_text = subinstr(note_text, "?", "", .) // this is just a check - doesn't end up doing anything. 
 	
-	* remove times: 
-	replace note_text = regexr(note_text, "[0-9]+:[0-9]+", "XTIMEX") 
-
-	* remove dates: 
-	replace note_text = ustrregexra(note_text, "(0[1-9]|1[012]|[1-9])[-/ \.]([1-9]|0[1-9]|[12][1-9]|3[01])[-/ \.](0[0-9]|1[1-9]|[20][01][0-9]|9[0-9])", "XDATEX") // this doesn't get 1999 dates quite right, but there shouldn't be any. 
-
-	* remove '-' among numbers 
+		* remove '-' among numbers 
 	replace note_text = ustrregexra(note_text, "-{2,}", "-", .) 
 	replace note_text = ustrregexra(note_text, "(?<=[0-9])-(?=[0-9])", " to ") 
 	replace note_text = ustrregexra(note_text, "(?<=[0-9])-", " ") 
 	replace note_text = ustrregexra(note_text, "-(?=[0-9])", " ") 
 
-	* remove - among words 
-	replace note_text = ustrregexra(note_text, "(?<=[A-Za-z])-(?=[A-Za-z])", " ") 
-	replace note_text = ustrregexra(note_text, "(?<=[A-Za-z]-(?=[0-9]))", " ") 
-	replace note_text = ustrregexra(note_text, "(?<=[0-9])-(?=[A-Za-z])", " ") 
-	replace note_text = ustrregexra(note_text, "-(?=[A-Za-z])", " ") 
-	replace note_text = ustrregexra(note_text, "(?<=[A-Za-z])-", " ") 
-
-	* periods at the end of sentences with no spaces 
-	* gets most of them. 
-	replace note_text = ustrregexra(note_text, "\.{2,}", "\.") 
-	replace note_text = ustrregexra(note_text, "(?<!\d)\.(?!\d)", " ") // period w/ no space -> "look backwards" AND "look forwards" regexes \d is decimal digits, so this is *not* those 
-	replace note_text = ustrregexra(note_text, "(?<=[A-Za-z])\.(?=\d)", " ") 
-	replace note_text = ustrregexra(note_text, "(?<=[0-9])\.(?=[A-Za-z])", " ") 
-	replace note_text = ustrregexra(note_text, "(?<=[0-9])\.(?=\s)", " ") 
-
-	* drop / in words -> not sure I really care whether it's in numbers or words 
-	replace note_text = ustrregexra(note_text, "/", " ") 
-	//ustrregexra(note_text, "(?<![0-9])/(?![0-9])", " ") 
-	replace note_text = ustrregexra(note_text, "\\", " ") 
-
-	* add a space after a number. 
-	replace note_text = ustrregexra(note_text, "(?<=[0-9])(?=[A-Za-z])", " ") 
-
-	* add a space before a number, BUT NOT BEFORE V 
-	replace note_text = ustrregexra(note_text, "(?<=[A-UW-Za-uw-z])(?=[0-9])", " ") // EXCLUDE V for V-CODES 
-
-	* fix lettercommaletter w/ no space 
-	replace note_text = ustrregexra(note_text,	"(?<=[A-Za-z]),(?=[A-Za-z])", " ") 
-
-	* replace %ile with % 
-	replace note_text = ustrregexra(note_text, "%ile", "%") 
-	* punctuation 
-	replace note_text = subinstr(note_text, "@", " ", .) 
-	replace note_text = subinstr(note_text, "#", " ", .) 
-	replace note_text = subinstr(note_text, ">", " ", .) 
-	replace note_text = subinstr(note_text, "<", " ", .) 
-	replace note_text = subinstr(note_text, "{", " ", .) 
-	replace note_text = subinstr(note_text, "}", " ", .) 
-	replace note_text = subinstr(note_text, ":", " ", .) 
-	replace note_text = subinstr(note_text, ",", " ", .) 
-	replace note_text = subinstr(note_text, ";", " ", .) 
-	replace note_text = subinstr(note_text, "+", " ", .) 
-	replace note_text = subinstr(note_text, "(", " ", .) 
-	replace note_text = subinstr(note_text, ")", " ", .) 
-	replace note_text = subinstr(note_text, "!", " ", .) 
-	replace note_text = subinstr(note_text, "[", " ", .) 
-	replace note_text = subinstr(note_text, "]", " ", .) 
-	replace note_text = subinstr(note_text, "*", " ", .) 
-	replace note_text = subinstr(note_text, "=", " ", .) 
-	* apostrophes are below. 
 	* need to remove " too 
-	replace note_text = ustrregexra(note_text, `"""', "" ) // requires double quoting with ` and " simultaneously 
-	replace note_text = subinstr(note_text, "&", " and ", .) 
-	* replace n spaces with single spaces: 
-	replace note_text = ustrregexra(note_text, " {2,}", " ", .) 
-	* replace n underscores with a single space 
-	replace note_text = ustrregexra(note_text, "_{2,}", " ", .) 
-	* possessives 
-	replace note_text = ustrregexra(note_text, "(?<=[A-Za-z])'s", "") 
-	replace note_text = ustrregexra(note_text, "(?<=[A-Za-z])`s", "") // someone does it with the other apostrophe? * cut out apostrophes 
+	X replace note_text = ustrregexra(note_text, `"""', "" ) // requires double quoting with ` and " simultaneously 
 
 
 	replace note_text = subinstr(note_text, "'", "", .)
@@ -97,28 +22,79 @@ need to get matches, then replace or substitute or whateve.
 
 function string_cleaner(x::String)
 		# replace(x, r""=>"")
+	x = lowercase(x)
 		# question marks
 	x = replace(x, r"\?{2,}"=>"?")
 	x = replace(x, r"\?"=>" ")
 	x = replace(x, r"-{2,}"=>"-")
+		# times 
+	x = replace(x, r"[0-9]+:[0-9]+"=>"XTIMEX")
+		# dates ⇒ doesn't quite get 1999 or -9 ending dates right.
+	x = replace(x, r"(0[1-9]|1[012]|[1-9])[-/ \.]([1-9]|0[1-9]|[12][1-9]|3[01])[-/ \.](0[0-9]|1[1-9]|[20][01][0-9]|9[0-9])" => "XDATEX")
 		# dashes
+			# this doesn't get - => to when surrounded by spaces
 	x = replace(x, r"(?<=[0-9])-(?=[0-9])"=>" to ") # positive lookbehind and positive lookahead to replace single - w/ " to "
 	x = replace(x, r"(?<=[0-9A-Za-z])-"=>" ")
 	x = replace(x, r"-(?=[0-9A-Za-z])"=>" ")
+	x = replace(x, r"(?<=[A-Za-z])-(?=[A-Za-z])"=>" ")
+	x = replace(x, r"(?<=[A-Za-z]-(?=[0-9]))"=>" ")
+	x = replace(x, r"(?<=[0-9])-(?=[A-Za-z])"=>" ")
+	x = replace(x, r"-(?=[A-Za-z])"=>" ")
+	x = replace(x, r"(?<=[A-Za-z])-"=>" ")
+	x = replace(x, r" - "=>" to ")
 		# periods 
 	x = replace(x, r"\.{2,}"=>".")
 	x = replace(x, r"(?<!\d)\.(?!\d)"=>"")
+	x = replace(x, r"(?<=[A-Za-z])\.(?=\d)"=>" ")
+	x = replace(x, r"(?<=[0-9])\.(?=[A-Za-z])"=>" ")
+	x = replace(x, r"(?<=[0-9])\.(?=\s)"=>" ")
+		# slash 
+	x = replace(x, r"/{1,}"=>" ")
+		# spaces after numbers: 
+	x = replace(x, r"?<=[0-9])(?=[A-Za-z])"=>" ")
+		# spaces before numbers, but beware v-codes 
+	x = replace(x, r"(?<=[A-UW-Za-uw-z])(?=[0-9])"=>" ")
+		# letter comma letter w/ no spaces 
+	x = replace(x, r"(?<=[A-Za-z]),(?=[A-Za-z])" => " ")
+		# %-ile to %
+	x = replace(x, r"%ile"=>"%") # %-ile may be missed.
+		# other punctuation
+	x = replace(x, r"@"=>" ")
+	x = replace(x, r"#"=>" ")
+	x = replace(x, r">"=>" ")
+	x = replace(x, r"<"=>" ")
+	x = replace(x, r"{"=>" ")
+	x = replace(x, r"}"=>" ")
+	x = replace(x, r":"=>" ")
+	x = replace(x, r","=>" ")
+	x = replace(x, r";"=>" ")
+	x = replace(x, r"+"=>" ")
+	x = replace(x, r"("=>" ")
+	x = replace(x, r")"=>" ")
+	x = replace(x, r"!"=>" ")
+	x = replace(x, r"["=>" ")
+	x = replace(x, r"]"=>" ")
+	x = replace(x, r"*"=>" ")
+	x = replace(x, r"="=>" ")
+	x = replace(x, r"&"=>" and ")
+	x = replace(x, r"_{2,}"=>" ")
+		# multiple spaces
+	x = replace(x, r" {2,}"=>" ")
+		# both apostrophes 
+	x = replace(x, r"(?<=[A-Za-z])'s"=>"")
+	x = replace(x, r"(?<=[A-Za-z])`s"=>"")
+
 	return x
 end
 
 
 
-
 	# TESTS.
-test1 = "something??? with? a long???? set of???strings??? of question??? marks????"
+test1 = "something??? with? a long???? set of???strings??? of question??? at 9:15 15:05 11:1 1:1 on 3/5/15 03/21/2019 6/2/1999 06/1/15 3/04/2019 "
 test2 = "some--times there are ---- long strings ?--- ??? ?  --- "
 test3 = "want. ...to r.....emove 2--5-word word-2----6 2-6 10 --- 41 --19----3-3-3-3-3"
-test4 = "v.345 v.v 3.6 7.v 8.w w.9 "
+test4 = "v.345 v.v 3.6 7.v 8.w w.9 1 - 5 1- 6 2 -3 3-6 ounces of Enfamil"
+test5 = ""
 
 # run tests
 string_cleaner(test1)
@@ -126,6 +102,9 @@ string_cleaner(test2)
 string_cleaner(test3)
 string_cleaner(test4)
 
+starr1 = [test1; test2; test3; test4]
+
+string_cleaner.(starr1)
 
 
 
