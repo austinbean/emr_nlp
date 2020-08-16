390691cb36ba387640391588ceedaec140aa063c
Austin Bean <austinbean@gmail.com>
2020-08-16 09:50:48 -0400

    changed backend in plotresults()

--- cnn_run_model.jl
+++ cnn_run_model.jl
@@ -83,7 +83,7 @@ function Run()
     # temp = Array{Float64,1}
     temp_mse = Float64[]
 	temp_rmse = Float64[]
-    for i = 1:2#arr.epos
+    for i = 1:arr.epos
         println(i)
         Flux.train!(loss, parms, trd, opt, cb = throttle(evalcb, 1)) #
         push!(temp_mse, testloss())
@@ -93,16 +93,16 @@ function Run()
     myResults.temp_rmse = temp_rmse
     
     function plotResults(df::DataFrame)
-        gr() #Create backend
+        plotly() #Create backend
         p1 = plot(df[:,1], markersize = 2, label = "Training", xlabel = "Number of Times Trained",ylabel = "Loss Function (MSE)")
         p2 = plot(df[:,2], markersize = 2, label = "Training", xlabel = "Number of Times Trained",ylabel = "Loss Function (RMSE)")
         # NB: tsd does not exist outside the scope of Run()
         h1 = histogram(m(tsd.data[1]),tsd.data[2], xlabel = "Food Value", ylabel = "Number of Occurences")
         CSV.write("./cnn_results.csv", myResults)
         plot(p1,p2,h1, layout = 3)
-        savefig(p1, "./CNN_test_mse.pdf")
-        savefig(p2, "./CNN_test_rmse.pdf")
-        savefig(h1, "./CNN_test_histogram.pdf")
+        savefig(p1, "./CNN_test_mse.png")
+        savefig(p2, "./CNN_test_rmse.png")
+        savefig(h1, "./CNN_test_histogram.png")
     end
 
     plotResults(myResults)
